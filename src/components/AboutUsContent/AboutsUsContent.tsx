7bc30a735d1338bfea6763b202c7c4c7b4399873
somebro <https://github.com/someBroYouKnow>
2025-06-02 16:21:11 +0530

    getting there about us

--- src/components/AboutUsContent/AboutsUsContent.tsx
+++ src/components/AboutUsContent/AboutsUsContent.tsx
@@ -9,16 +9,16 @@ import { extractContentByKey } from '@/utils/common.util';
 import Link from 'next/link';
 import '../../css/about.css';
 
- 
+
 interface AboutUsContentProps {
     content: any;
     tag: any;
     team: any;
 }
 
-export const AboutUsContent = ({ content, tag, team }: AboutUsContentProps) => { 
+export const AboutUsContent = ({ content, tag, team }: AboutUsContentProps) => {
     const isMobile = useIsMobile(1000);
-    
+
 
 
     const AboutUsMottoArray = [
@@ -39,7 +39,7 @@ export const AboutUsContent = ({ content, tag, team }: AboutUsContentProps) => {
     const aboutText = extractContentByKey(content, 'about-us'),
         usText = extractContentByKey(content, 'us');
 
- 
+
 
     return (
         <>
@@ -89,7 +89,7 @@ export const AboutUsContent = ({ content, tag, team }: AboutUsContentProps) => {
             <div className="about-main-container">
                 <div className="about-motto">
                     {
-                        AboutUsMottoArray.map((Motto, index:number) =>
+                        AboutUsMottoArray.map((Motto, index: number) =>
                             <div className="about-motto-box" key={index}>
                                 <div className="motto-title">{Motto.title}</div>
                                 <div className="motto-description">{Motto.description}</div>
@@ -105,25 +105,8 @@ export const AboutUsContent = ({ content, tag, team }: AboutUsContentProps) => {
                 </div>
 
                 <div className="about-people-tile-container">
-                    {team?.map((person: any, index:number) => (
-                        <div key={index} className="person-tile">
-                            <img
-                                src={person.userImage}
-                                alt={person.name}
-                                className="person-image"
-                            />
-                            <div className="person-info">
-                                <div className="person-credentails">
-                                    <div className="person-name">{person.name}</div>
-                                    <div className="person-designation">{person.jobTitle}</div>
-                                </div>
-                                <div className="social-links-favicon">
-                                    <a href={person?.personSocials?.linkedIn}><img src="/favicon/linkedin.svg" alt="" /></a>
-                                    {/* <a href={person.personSocials.instagram}><img src="/favicon/instagram.svg" alt="" /></a>
-                <a href={person.personSocials.facebook}><img src="/favicon/facebook.svg" alt="" /></a> */}
-                                </div>
-                            </div>
-                        </div>
+                    {team?.map((person: any, index: number) => (
+                        <PersonTile key={index} person={person} />
                     ))}
                 </div>
             </div>
@@ -134,3 +117,56 @@ export const AboutUsContent = ({ content, tag, team }: AboutUsContentProps) => {
         </>
     )
 }
+
+const PersonTile = ({ person }: { person: any }) => {
+    return (
+        <div className="person-tile">
+            <div className="person-image-container">
+                <img
+                    src={person.userImage}
+                    alt={person.name}
+                    className="person-image"
+                />
+            </div>
+            <div className="person-info">
+                <div className="person-credentials">
+                    <div className="person-name">{person.name}</div>
+                    <div className="person-designation">{person.jobTitle}</div>
+                </div>
+                <div className="social-links-favicon">
+                    {person?.personSocials?.linkedIn && (
+                        <a 
+                            href={person.personSocials.linkedIn} 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            aria-label={`${person.name}'s LinkedIn profile`}
+                        >
+                            <img src="/favicon/linkedin.svg" alt="LinkedIn" />
+                        </a>
+                    )}
+                    {/* Uncomment these if you want to add other social links */}
+                    {/* {person?.personSocials?.instagram && (
+                        <a 
+                            href={person.personSocials.instagram} 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            aria-label={`${person.name}'s Instagram profile`}
+                        >
+                            <img src="/favicon/instagram.svg" alt="Instagram" />
+                        </a>
+                    )}
+                    {person?.personSocials?.facebook && (
+                        <a 
+                            href={person.personSocials.facebook} 
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                            aria-label={`${person.name}'s Facebook profile`}
+                        >
+                            <img src="/favicon/facebook.svg" alt="Facebook" />
+                        </a>
+                    )} */}
+                </div>
+            </div>
+        </div>
+    );
+};
