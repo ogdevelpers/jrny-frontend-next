9554ec1f44f48ef82baa148d71cd5923a2d6058e
somebro <https://github.com/someBroYouKnow>
2025-06-04 18:20:14 +0530

    changes to video

--- src/components/ExpandingVideo/ExpandingVideo.tsx
+++ src/components/ExpandingVideo/ExpandingVideo.tsx
@@ -68,8 +68,11 @@ useGSAP(() => {
         trigger: '.expanding-video-container',
         start: 'top 45%',
         end: 'bottom bottom',
-        scrub: true,
-      }
+        scrub: 1.1,
+      },
+      defaults: {
+        ease: 'sine.out',
+      },
     });
 
     // Video expansion animation with fromTo to prevent shrinking
@@ -79,7 +82,7 @@ useGSAP(() => {
         boxShadow: '0px 4px 24px 0px #FF5B0066'
       },
       {
-        width: 'min(90vw, 1200px)',
+        width: 'min(80vw, 1400px)',
         boxShadow: '0px 8px 48px 0px #FF5B0099',
       }
     );
@@ -91,8 +94,8 @@ useGSAP(() => {
         x: 0 // Start from CSS position
       },
       {
-        y: '20vh',
-        xPercent: -100, // Move further left (negative value moves left from CSS transform)
+        y: '21vh',
+        xPercent: -110, // Move further left (negative value moves left from CSS transform)
       }, 0
     );
 
@@ -102,8 +105,8 @@ useGSAP(() => {
         x: 0 // Start from CSS position
       },
       {
-        y: '20vh',
-        xPercent: 100, // Move further right (positive value moves right from CSS transform)
+        y: '21vh',
+        xPercent: 110, // Move further right (positive value moves right from CSS transform)
       }, 0
     );
 
