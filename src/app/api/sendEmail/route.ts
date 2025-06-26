2fa3fca8c7c0e438408dd34b96dfb6fee7699943
og developers <developers506@gmail.com>
2025-06-26 21:46:58 +0530

    Added runtime for api sendemail

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { EmailTemplate } from '@/components/EmailTemplate/EmailTemplate';
 import { NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
