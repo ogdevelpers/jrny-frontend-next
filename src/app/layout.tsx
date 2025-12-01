65bb277ed6ae9f81be3473bcd808f7b356a313d8
og developers <developers506@gmail.com>
2025-12-02 00:11:11 +0530

    Added gtm tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,13 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MXMLQB9B');`}
+        </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-JSZ3Q3FG1E"
           strategy="afterInteractive"
@@ -46,6 +53,14 @@ export default async function RootLayout({
         </Script>
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MXMLQB9B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {/* <SmoothWrapper> */}
         <div className="pinMe" id="smooth-wrapper">
           <Header header={headerData} />
